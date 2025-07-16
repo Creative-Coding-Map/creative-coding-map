2123a6efda6bde8baed23eb23b5acdd22403bf3c
ricardomatias <ricardo.matias0@gmail.com>
2025-07-16 11:07:12 +0200

    domain fixing attempt

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,7 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/',
+    base: process.env.NODE_ENV === 'production' ? '/creative-coding-map/' : '/',
     plugins: [
         viteReact(),
         svgr(),
