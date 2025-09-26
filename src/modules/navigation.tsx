c4252c87f8b2813f1def9cfdd05a318d43593126
ricardomatias <ricardo.matias0@gmail.com>
2025-09-26 14:04:27 +0200

    fix build

--- src/modules/navigation.tsx
+++ src/modules/navigation.tsx
@@ -1,11 +1,7 @@
 import clsx from 'clsx';
-import { useAtom } from 'jotai';
 import { Link } from 'wouter';
-import { store } from '@/state/store';
-import { showSearchAtom } from '@/state/model';
 
 export const Navbar = () => {
-    const [showSearch, setShowSearch] = useAtom(showSearchAtom, { store });
     return (
         <nav className="flex justify-between items-center absolute top-0 left-0 right-0 ccm-padding">
             <section className="flex items-center gap-4">
@@ -23,17 +19,6 @@ export const Navbar = () => {
                 <Link className={(active) => clsx('link type-header z-20 ccm-colors-fg', active && 'active')} href="/breakdowns">
                     Breakdowns
                 </Link>
-                <button
-                    className="link type-header z-20 ccm-colors-fg"
-                    onClick={(event) => {
-                        event.preventDefault();
-                        event.stopPropagation();
-
-                        setShowSearch(!showSearch);
-                    }}
-                >
-                    Search
-                </button>
             </section>
         </nav>
     );
