da171e45ae3479ee07793a71d437517286bd1bf9
ricardomatias <ricardo.matias0@gmail.com>
2025-09-26 17:56:38 +0200

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,7 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: process.env.NODE_ENV === 'production' ? '/creative-coding-map/' : '/',
+    base: '/',
     plugins: [
         viteReact(),
         svgr(),
